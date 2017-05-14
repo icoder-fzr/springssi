cffc71df2fb8b348341d34cf41522470f1b01b59
fengzirong <237224316@qq.com>
2017-05-14 14:59:01 +0800

    初始化spring-boot

---
+++ springssi/src/main/java/cn/spring/boot/start/SampleController.java
@@ -0,0 +1,26 @@
+package cn.spring.boot.start;
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by fengzirong on 2017-05-14.
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(SampleController.class,args);
+    }
+}
